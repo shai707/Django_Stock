b30a4e09e0517399af72495a3554c4cb11171e04
Shai Goldenberg <53555319+shai707@users.noreply.github.com>
2025-05-02 12:28:51 +0300

    Update settings.py

--- stocks/stocks/settings.py
+++ stocks/stocks/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7#b26g(bkutg^3h^rk$b7+a(uoulfr9*1eqo1nf@xr!yb=*kyo'
+SECRET_KEY = '*****'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
